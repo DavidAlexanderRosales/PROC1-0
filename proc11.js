8b8160590c6a0892d8f63d4f1fcd1c505876de77
DavidAlexanderRosales <99056780+DavidAlexanderRosales@users.noreply.github.com>
2022-02-21 20:08:04 -0600

    Add files via upload

---
+++ proc11.js
@@ -0,0 +1,18 @@
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+
+seaImg = loadImages("sea,png")
+shipImg1 = loadAnimation (//);
+SpriteName. addimages (seaimag);
+}
